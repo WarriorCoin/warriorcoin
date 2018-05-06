f85ca8548398de3dbaf66ba64e66d49683f78a2f
warriorcoin <m1rc098@outlook.com>
2018-05-06 13:46:42 -0700

    ready for testnet mining with 2 computers

--- README.md
+++
@@ -1,2 +0,0 @@
-# warriorcoin
-A scrypt cryptocurrency for the Mission San Jose High School community.
--- src/obj/build.h
+++
@@ -1,2 +0,0 @@
-#define BUILD_DESC "v0.6.1-686-ga19eb2d-dirty"
-#define BUILD_DATE "2013-05-21 11:13:29 +0200"
